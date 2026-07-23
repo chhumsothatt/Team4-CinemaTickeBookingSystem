4304b1b2417c7eb2f293e1dd79235fde1b4c6523
sothatt <147292652+sothatt@users.noreply.github.com>
2026-07-23 23:02:07 +0700

    editcode

--- client/index.php
+++ client/index.php
@@ -3,38 +3,6 @@ session_start();
 include("../include/header.php");
 include("../include/navbar.php");
 ?>
-<<<<<<< HEAD
-<!-- NAVBAR -->
-<nav class="navbar navbar-expand-lg sticky-top py-3">
-  <div class="container">
-    <a class="navbar-brand font-display fs-3 d-flex align-items-center gap-2" href="#top">
-      <span class="bulb"></span><span class="text-marquee">CINÉ</span><span class="fs-6 fw-normal ms-1">MARQUEE</span>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMain">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navMain">
-      <ul class="navbar-nav mx-auto gap-lg-4 fw-semibold text-center">
-        <li class="nav-item"><a class="nav-link text-marquee" href="#movies">ភាពយន្ត</a></li>
-        <li class="nav-item"><a class="nav-link" href="#booking">កក់សំបុត្រ</a></li>
-        <li class="nav-item"><a class="nav-link" href="#history">ប្រវត្តិកក់</a></li>
-      </ul>
-      <div class="d-flex gap-2 mt-3 mt-lg-0">
-        <?php 
-        // session_start();
-        if(isset ($_SESSION['role'] )){ 
-          echo '<a href="../login.php" class="btn btn-outline-cinema px-3">ចូល / Login</a>
-          <a href="../register.php" class="btn btn-marquee px-3">ចុះឈ្មោះ</a>';
-          
-         }else{
-            echo '<a id="btnLogout" class="btn btn-danger px-3">ចេញ / Logout</a>';
-         }
-         if(isset($_SESSION['role']) && $_SESSION['role'] === 'admin'){
-          echo '<a href="../admin/dashboard.php" class="btn btn-primary border-0 px-3">Dashboard</a>';
-         }
-         ?>
-=======
->>>>>>> 7e8816381c1bd2ef16771877dc6c71f9214439cb
 
 <div class="filmstrip"></div>
 
